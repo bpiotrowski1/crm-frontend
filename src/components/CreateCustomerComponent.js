39e03e41ebd436f0f7cc6fcb3d5575eab10187a4
bpiotrowski1 <baej.p26@gmail.com>
2022-03-27 18:41:47 +0200

    Added support for settings to field names

--- src/components/CreateCustomerComponent.js
+++ src/components/CreateCustomerComponent.js
@@ -1,22 +1,43 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import CustomerService from "../service/CustomerService";
+import SettingService from "../service/SettingService";
 
 const CreateCustomerComponent = () => {
 
     const [name, setName] = useState('');
+    const [firstField, setFirstField] = useState('');
+    const [firstFieldName, setFirstFieldName] = useState('');
+    const [secondField, setSecondField] = useState('');
+    const [secondFieldName, setSecondFieldName] = useState('');
+    const [thirdField, setThirdField] = useState('');
+    const [thirdFieldName, setThirdFieldName] = useState('');
+    const [fourthField, setFourthField] = useState('');
+    const [fourthFieldName, setFourthFieldName] = useState('');
+    const [textArea, setTextArea] = useState('');
+    const [textAreaName, setTextAreaName] = useState('');
     const navigate = useNavigate();
+
+    useEffect(() => {
+        SettingService.getSettings().then(response => {
+            setFirstFieldName(response.data.firstFieldName);
+            setSecondFieldName(response.data.secondFieldName);
+            setThirdFieldName(response.data.thirdFieldName);
+            setFourthFieldName(response.data.fourthFieldName);
+            setTextAreaName(response.data.textAreaName);
+        }).catch(error => {
+            console.log(error);
+        })
+    })
 
     const saveCustomer = (e) => {
         e.preventDefault();
 
-        const customer = { name };
+        const customer = { name, firstField, secondField, thirdField, fourthField, textArea };
 
-        CustomerService.createCustomer(customer).then(response => {
-            navigate('/');
-        }).catch(error => {
+        CustomerService.createCustomer(customer).then(navigate('/')).catch(error => {
             console.log(error);
-        });
+        })
     }
 
     return (
@@ -24,10 +45,34 @@ const CreateCustomerComponent = () => {
             <br/>
             <h2>Create customer</h2>
             <hr/>
-            <form className='ui form'>
+            <form id='form' className='ui form'>
                 <div className='field'>
                     <label>Name:</label>    
                     <input placeholder='Name' name='name' className='form-control' value={name} onChange={(e) => setName(e.target.value)}/>
+                </div>
+                <div className='two fields'>
+                    <div className='field'>
+                        <label>{firstFieldName}:</label>    
+                        <input placeholder={firstFieldName} name='firstField' className='form-control' value={firstField} onChange={(e) => setFirstField(e.target.value)}/>
+                    </div>
+                    <div className='field'>
+                        <label>{secondFieldName}:</label>    
+                        <input placeholder={secondFieldName} name='secondField' className='form-control' value={secondField} onChange={(e) => setSecondField(e.target.value)}/>
+                    </div>
+                </div>
+                <div className='two fields'>
+                    <div className='field'>
+                        <label>{thirdFieldName}:</label>    
+                        <input placeholder={thirdFieldName} name='thirdField' className='form-control' value={thirdField} onChange={(e) => setThirdField(e.target.value)}/>
+                    </div>
+                    <div className='field'>
+                        <label>{fourthFieldName}:</label>    
+                        <input placeholder={fourthFieldName} name='fourthField' className='form-control' value={fourthField} onChange={(e) => setFourthField(e.target.value)}/>
+                    </div>
+                </div>
+                <div className='field'>
+                    <label>{textAreaName}:</label>
+                    <textarea className='text-area w-100' rows='10' name='textArea' id='textarea' value={textArea} onChange={(e) => setTextArea(e.target.value)}></textarea>
                 </div>
                 <br/>
                 <button className='ui positive button' onClick={(e) => saveCustomer(e)}>Save</button>
--- src/components/CustomersComponent.js
+++ src/components/CustomersComponent.js
@@ -1,13 +1,29 @@
 import React, { useEffect, useState } from 'react';
 import CustomerService from '../service/CustomerService';
+import SettingService from '../service/SettingService';
 import BreadcrumComponent from './BreadcrumbComponent';
 
 const CustomersComponent = () => {
 
     const [customers, setCustomers] = useState([]);
+    const [firstFieldName, setFirstFieldName] = useState('');
+    const [secondFieldName, setSecondFieldName] = useState('');
+    const [thirdFieldName, setThirdFieldName] = useState('');
+    const [fourthFieldName, setFourthFieldName] = useState('');
+    const [textAreaName, setTextAreaName] = useState('');
 
     useEffect(() => {
         getCustomers();
+        
+        SettingService.getSettings().then(response => {
+            setFirstFieldName(response.data.firstFieldName);
+            setSecondFieldName(response.data.secondFieldName);
+            setThirdFieldName(response.data.thirdFieldName);
+            setFourthFieldName(response.data.fourthFieldName);
+            setTextAreaName(response.data.textAreaName);
+        }).catch(error => {
+            console.log(error);
+        })
     }, [])
 
     const getCustomers = () => {
@@ -24,12 +40,12 @@ const CustomersComponent = () => {
             <table id='customers' className='ui celled table striped'>
                 <thead>
                     <tr className='thead'>
-                        <th>Nazwa</th>
-                        <th>Pierwsze</th>
-                        <th>Drugie</th>
-                        <th>Trzecie</th>
-                        <th>Czwarte</th>
-                        <th>Pole tekstowe</th>
+                        <th>Name</th>
+                        <th>{firstFieldName}</th>
+                        <th>{secondFieldName}</th>
+                        <th>{thirdFieldName}</th>
+                        <th>{fourthFieldName}</th>
+                        <th>{textAreaName}</th>
                     </tr>
                 </thead>
                 <tbody>
